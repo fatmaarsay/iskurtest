671ef851bc248d4c498103cc6d92ade5d8f873a3
Fatma Arsay <fatmaarsay200@outlook.com>
2025-12-24 04:06:53 +0300

    Veritabanı bağlantısı ve hata yönetimi iyileştirildi
    DBConnection.cs'de bağlantı dizesi sadeleştirildi ve gereksiz using'ler kaldırıldı. DbRepository.cs'de Entity Framework bağımlılığı kaldırıldı, MySQL kütüphanesi kullanıldı. Tüm temel veritabanı işlemlerine try-catch blokları eklenerek hata yönetimi güçlendirildi. Kodun okunabilirliği ve güvenliği artırıldı.

--- WinFormsApp1/DataBase/DBConnection.cs
+++ WinFormsApp1/DataBase/DBConnection.cs
@@ -1,10 +1,4 @@
 ﻿using MySql.Data.MySqlClient;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Text;
-using System.Windows.Forms;
-using MySql.Data.MySqlClient;
 
 namespace WinFormsApp1.DataBase
 {
@@ -12,7 +6,7 @@ namespace WinFormsApp1.DataBase
     public static class DbConnection
     {
         private static readonly string _connectionString =
-            "Server=localhost;Database=iskur;Uid=root;Pwd=BURAYA KENDİ MYSQL ŞİFRENİ YAZ(HENÜZ ŞİFRENİ OLUŞTURMADIYSAN ms123456. YAPTIM BEN KOD PAYLAŞTIĞIMIZDA ORTAK ŞİFRE KOLAYLIK SAĞLAR).";
+            "Server=localhost;Database=iskur;Uid=root;Pwd=ms123456.";
 
         public static MySqlConnection GetConnection()
         {
@@ -21,7 +15,7 @@ namespace WinFormsApp1.DataBase
             try
             {
                 connection.Open();
-                
+
                 return connection;
             }
             catch (MySqlException ex)
@@ -29,13 +23,13 @@ namespace WinFormsApp1.DataBase
                 MessageBox.Show("Veritabanı bağlantı hatası: " + ex.Message);
                 throw;
 
-               
+
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Genel bağlantı hatası: " + ex.Message);
                 throw;
-                
+
             }
         }
     }
